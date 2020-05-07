67d5bfa823af77e248c3f49fca41c82daae241ae
Wquarks <59671217+Wquarks@users.noreply.github.com>
2020-05-07 17:11:11 +0200

    Add files via upload

---
+++ target/scala-2.13/routes/main/controllers/routes.java
@@ -0,0 +1,20 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:C:/Users/William/Desktop/play/play-java-seed/conf/routes
+// @DATE:Thu May 07 15:59:00 CEST 2020
+
+package controllers;
+
+import router.RoutesPrefix;
+
+public class routes {
+  
+  public static final controllers.ReverseHomeController HomeController = new controllers.ReverseHomeController(RoutesPrefix.byNamePrefix());
+  public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets(RoutesPrefix.byNamePrefix());
+
+  public static class javascript {
+    
+    public static final controllers.javascript.ReverseHomeController HomeController = new controllers.javascript.ReverseHomeController(RoutesPrefix.byNamePrefix());
+    public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets(RoutesPrefix.byNamePrefix());
+  }
+
+}
